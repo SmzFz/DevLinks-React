763efb310d0d5dfa977203104f6ad241700a69b3
SmzFz <felipesalmazo123@gmail.com>
2023-08-11 10:22:14 -0300

    Desenvolvido profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
 import Profile from "../components/profile/profile";
+import { useState } from "react";
 
-export default function App() {
-    return(
-        <>
-<Profile></Profile>
+import "./App.css";
 
-        </>
-    )
+import imgDarkMode from "../img/avatar.png"
+import imgLightMode from "../img/avatar-light.jpg"
+
+const App = () => {
+    const [isLightMode,setIsLightMode] = useState(false)
+const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+    const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
+  return (
+    <div>
+      <Profile imgAlt={imgAlt} imgSrc={imgSrc}>@Salmazo.f</Profile>
+    </div>
+  );
 };
+
+export default App;
