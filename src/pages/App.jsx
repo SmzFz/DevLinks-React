46ddd0e7170fbc43918b49ade66626453943c64a
SmzFz <felipesalmazo123@gmail.com>
2023-08-11 11:05:38 -0300

    switch button OK

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,18 +1,22 @@
 import Profile from "../components/profile/profile";
 import { useState } from "react";
+import Switch from "../components/switch/switch";
 
 import "./App.css";
 
-import imgDarkMode from "../img/avatar.png"
-import imgLightMode from "../img/avatar-light.jpg"
+import imgDarkMode from "../img/avatar.png";
+import imgLightMode from "../img/avatar-light.jpg";
 
 const App = () => {
-    const [isLightMode,setIsLightMode] = useState(false)
-const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
-    const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
+  const [isLightMode, setIsLightMode] = useState(false);
+  const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+  const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro";
   return (
-    <div>
-      <Profile imgAlt={imgAlt} imgSrc={imgSrc}>@Salmazo.f</Profile>
+    <div id="container">
+      <Profile imgAlt={imgAlt} imgSrc={imgSrc}>
+        @Salmazo.f
+      </Profile>
+      <Switch></Switch>
     </div>
   );
 };
